64914fb8dd0d5c1284005be96d16caa67c6f6b51
atifaziz1 <aatif.aziz1@gmail.com>
2022-11-21 14:49:03 +0300

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  DemoExample
+//
+
+import Foundation
---
+++ ios/Demo.swift
@@ -0,0 +1,8 @@
+@objc(Demo)
+class Demo: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
